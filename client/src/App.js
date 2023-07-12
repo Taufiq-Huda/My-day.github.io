5e727a674b23e7ae93b9c41a7833f2b9be8de7d0
taufir-huda <110535361+taufir-huda@users.noreply.github.com>
2023-07-12 20:44:31 +0600

    Version 1.0.2 ready

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,8 @@ import NavBar from "./components/NavBar";
 import NewPage from "./components/NewPage";
 
 function App() {
-  localStorage.setItem("auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5OTQ5ZjdhOTQ0N2QxMDY5N2M5ZDJkIn0sImlhdCI6MTY4Nzc2NzU0M30.XZtV5XxVuluXyBroeUU2DL1EfHA8aD-H6m1pf2_AqCs");
-  // localStorage.setItem("auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5YzcyZWIwZDdjYzI3ZDhmZTg2NTAwIn0sImlhdCI6MTY4Nzk3NDYzNn0.6bnYMT-N1uWwnjvFHvZMFybzPO23yS-OoMQNWRmed6w");
+  // localStorage.setItem("auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5OTQ5ZjdhOTQ0N2QxMDY5N2M5ZDJkIn0sImlhdCI6MTY4Nzc2NzU0M30.XZtV5XxVuluXyBroeUU2DL1EfHA8aD-H6m1pf2_AqCs");
+  localStorage.setItem("auth-token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjQ5YzcyZWIwZDdjYzI3ZDhmZTg2NTAwIn0sImlhdCI6MTY4Nzk3NDYzNn0.6bnYMT-N1uWwnjvFHvZMFybzPO23yS-OoMQNWRmed6w");
   return (
     <div className="App">
       <NavBar />
